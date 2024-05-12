c7b5ae4409a1d4a3590678450c127c11b97149f5
Orton Bright <ortonb110@gmail.com>
2024-05-12 15:47:04 +0000

    Audiophile website initial setup. This is a third attempt for the frontend mentor challenge

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className=" w-full h-[100vh] flex justify-center items-center text-black">
+        <h1 className="capitalize">hello world!</h1>
+    </main>
+  );
+}
